9b187593fb1072411fd5ae4159c4d40dc4ba7895
Thomas Stewart <thomas.w.stewart@biola.edu>
2017-11-07 20:54:39 -0800

    Create solution.java

---
+++ java/F-loops/solution.java
@@ -0,0 +1,25 @@
+public class TestClass {
+    public static void main(String[] args)
+    {
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
+public class TestClass {
+    public static void main(String[] args)
+    {
+        int i = 0;
+        while (i <= 100) {
+          if (i % 2 == 0) {
+            System.out.println(i);
+          }
+          i += 1;
+        }
+    }
+}
+
+
