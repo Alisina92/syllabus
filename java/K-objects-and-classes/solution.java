ae25537208498d18f20df7eb0bf8d71375454c0f
Thomas Stewart <thomas.w.stewart@biola.edu>
2017-11-21 16:30:17 -0800

    Create solution.java

---
+++ java/K-objects-and-classes/solution.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+    
+        Animal frog = new Animal();
+
+        System.out.println(frog.sound);
+    }
+}
+
+// This class goes in its own separate file
+public class Animal {
+
+    String name = "Frog";
+    int numberOfLegs = 4;
+    String sound = "Ribbit!";
+    String color = "Green";
+    boolean isPreditor = false;
+    
+}
