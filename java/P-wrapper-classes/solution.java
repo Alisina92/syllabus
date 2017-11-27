60196a5ac6c3b97d121a8275e71fae8fee1f62e1
Thomas Stewart <thomas.w.stewart@biola.edu>
2017-11-27 13:58:17 -0800

    Create solution.java

---
+++ java/P-wrapper-classes/solution.java
@@ -0,0 +1,5 @@
+1. 
+Autoboxing is the automatic conversion that the Java compiler makes between the 
+primitive types and their corresponding object wrapper classes. 
+For example, converting an int to an Integer, a double to a Double, and so on. 
+If the conversion goes the other way, this is called unboxing.
