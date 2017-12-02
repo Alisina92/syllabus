6cd50bd8eeb7cd5267778eb7b70851b7d0309b2e
Thomas Stewart <thomas.w.stewart@biola.edu>
2017-12-02 02:04:42 -0800

    Update solution.java

--- java/E-loops/solution.java
+++ java/E-loops/solution.java
@@ -28,6 +28,17 @@ public class TestClass {
 public class TestClass {
     public static void main(String[] args)
     {
+        int[] intArray = {1,2,3,4,5,6,7,8,9,10};
+        for (int i : intArray) {
+            System.out.println("i = " + i);
+        }
+    }
+}
+
+4.
+public class TestClass {
+    public static void main(String[] args)
+    {
         
        for (int i = 1; i <= 10; i++) {
             System.out.print(i + " ");
