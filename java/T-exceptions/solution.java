1a0a7420659d7c93a0837eddb66b7128d1c17112
Thomas Stewart <thomas.w.stewart@biola.edu>
2017-12-08 04:00:38 -0800

    Update solution.java

--- java/T-exceptions/solution.java
+++ java/T-exceptions/solution.java
@@ -1,6 +1,5 @@
 1.
 public class Main {
-
     public static void main(String[] args) {
 
         try {
@@ -11,5 +10,21 @@ public class Main {
         }
 
     }
+}
 
+2.
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            int x = 10;
+        }
+        catch (ArithmeticException e) { // the code in this block will only run when an exception has been thrown
+            System.out.println("An exception occurred: " + e);
+        }
+        finally { // the code in this block will always run, regardless if an exception was thrown or not
+            System.out.println("You are in the finally block!");
+        }
+
+    }
 }
