5f9885f9563e214698186ec6ef63720336b636a6
Thomas Stewart <thomas.w.stewart@biola.edu>
2017-12-08 06:47:54 -0800

    Update solution.java

--- java/T-exceptions/solution.java
+++ java/T-exceptions/solution.java
@@ -28,3 +28,48 @@ public class Main {
 
     }
 }
+
+3.
+import java.util.Scanner;
+
+public class Main {
+
+    public static double totalMoney = 100.00;
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("******************");
+            System.out.println("Your total is: " + totalMoney);
+            System.out.println("******************");
+
+            System.out.println("How much would you like to withdraw?");
+
+            double moneyToWithdraw = scanner.nextDouble();
+
+            // will need to wrap this in a try/catch
+            try {
+                withdrawMoney(moneyToWithdraw);
+            }
+            catch (Exception e) {
+                System.out.println("******************");
+                System.out.println("ERROR: An error has occurred! Please try again.");
+                System.out.println("******************");
+                System.out.println();
+            }
+        }
+
+    }
+
+    public static void withdrawMoney(double moneyToWithdraw) throws Exception {
+        if (moneyToWithdraw > totalMoney) {
+            // throw an exception here, because we do not want the user to withdraw more money than he has
+            throw new Exception();
+        }
+
+        totalMoney = totalMoney - moneyToWithdraw;
+    }
+
+}
