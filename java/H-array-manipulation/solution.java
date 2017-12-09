077578bc110e4b76c43c5f919731f6a27199a3dc
Thomas Stewart <thomas.w.stewart@biola.edu>
2017-12-09 04:10:11 -0800

    Update solution.java

--- java/H-array-manipulation/solution.java
+++ java/H-array-manipulation/solution.java
@@ -28,13 +28,10 @@ public class Main
     public static void main(String[] args) {
         int[] numbers = {1,24,67,32,9,54,2};
 
-        int biggestNumber = 0;
         ArrayList<Integer> myList = new ArrayList<>();
 
         for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] > biggestNumber) {
-                myList.add(numbers[i]);
-            }
+            myList.add(numbers[i]);
         }
 
         Collections.sort(myList);
